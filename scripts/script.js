f7a706fc33195227f3069d59741ab951c4c39384
aurimasvalys <aurimasvalys@gmail.com>
2020-04-22 21:11:12 +0300

    Priskirti vardai script dalims

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+/* Bendras scriptas: */
+
+
+
+
+
+/* Aurimas Valys script kodas: */
+
+
+
+
+
+/* Tomas Pacesa script kodas: */
+
+
+
+
+
+/* Zbignev Rakovskij script kodas: */
+
+
+
+
+
+/* Aurimas Surgelas script kodas: */

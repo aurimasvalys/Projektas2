898841ec93ca3ea26417bc992a4a6d3b119515cc
Aurimas-Samirua <asurgelas@gmail.com.>
2020-04-23 19:26:52 +0300

    visos naujienos stylius

--- visosnaujienos.php
+++ visosnaujienos.php
@@ -12,7 +12,76 @@
 </nav>
 <h2 class="center-align">cia yra visos naujienos</h2>
 
-<!-- Čia puslapio, kur yra visos naujienos vidinė dalis -->
+<h2 id="fet">News</h2>
+
+
+<div class="konteineris">
+
+
+    <div class="tema1">
+        <img class="nuotraukos" src="https://picsum.photos/300" alt="">
+
+        <div class=dalis1>
+            <h3 id="let">Sportas</h3>
+            <p class="aprasymas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+                quia
+                accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
+                perspiciatis. Autem ea perspiciatis sunt?</p>
+            <br>
+            <p class="aprasymas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+                quia
+                accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
+                perspiciatis. Autem ea perspiciatis sunt?</p>
+        </div>
+        <div><input class="dvs" type="submit" name="submit" value="Open"></div>
+
+    </div>
+
+
+
+    <div class="tema1">
+        <img class="nuotraukos" src="https://picsum.photos/301" alt="">
+
+        <div class=dalis1>
+            <h3 id="let">Politika</h3>
+            <p class="aprasymas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+                quia
+                accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
+                perspiciatis. Autem ea perspiciatis sunt?</p>
+            <br>
+            <p class="aprasymas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+                quia
+                accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
+                perspiciatis. Autem ea perspiciatis sunt?</p>
+
+
+        </div>
+        <div><input class="dvs" type="submit" name="submit" value="Open"></div>
+    </div>
+
+
+
+
+    <div class="tema1">
+        <img class="nuotraukos" src="https://picsum.photos/302" alt="">
+
+        <div class=dalis1>
+            <h3 id="let">Laisvalaikis</h3>
+            <p class="aprasymas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+                quia
+                accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
+                perspiciatis. Autem ea perspiciatis sunt?</p>
+            <br>
+            <p class="aprasymas">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+                quia
+                accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
+                perspiciatis. Autem ea perspiciatis sunt?</p>
+        </div>
+        <div><input class="dvs" type="submit" name="submit" value="Open"></div>
+    </div>
+
+</div>
+
 
 
 

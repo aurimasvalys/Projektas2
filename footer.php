26209a8e0edd65fdfe427640c1bae3a9e4cc4b1d
aurimasvalys <aurimasvalys@gmail.com>
2020-04-23 12:28:27 +0300

    pataisyta nuoroda i script js faila

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <!-- Čia pasikartojanti visuose puslapiuose apatinė dalis -->
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="scripts/script.j"></script>
+    <script src="scripts/script.js"></script>
 
 </body>
 </html>

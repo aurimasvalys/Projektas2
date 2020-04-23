69f4d273fa72ce5dc2a850f7fdeb763bfc241601
zrakoweb <zbignev.rakovskij@yahoo.com>
2020-04-23 21:50:59 +0300

    trecias lapas redaguotas

--- vienanaujiena.php
+++ vienanaujiena.php
@@ -6,5 +6,108 @@
 <!-- Čia puslapio, kur yra viena naujiena vidinė dalis -->
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head >
+<h1 class="antraste container">Pagrindine naujiena</h1>
+
+<body>
+
+
+<div class="fonas row container">
+   <img class = " aukstis col s12" src="https://picsum.photos/50" alt="">
+   <div>Lorem ip"sum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+
+   </div>
+
+  
+
+</div >
+
+<!-- <div class="down container row">
+
+      <div class=" pirmasParagrafas row col s12 m6 l4 offset-l2">
+          
+        <img src="https://picsum.photos/100" alt="">
+             <h2 class="naujiena1">Antra naujiena</h2>
+               <a href=""> 
+                   
+               cia linkas i 
+           <ul><maps class="google.com"></maps></ul>
+       </a>
+
+      </div>
+
+      
+
+      <div class="antrasParagrafas col s12 m6 l4">
+        <img src="https://picsum.photos/100" alt="">
+        <h2 class="naujiena1">trecia naujiena</h2>
+        <a href="">cia kitas  linkas i 
+           <ul><maps class="google.com"></maps></ul>
+       </a>
+      </div>
+
+      
+    </div> -->
+
+    
+
+    <div class="row container">
+    <div class=" row col s6 m4">
+      <div class="card blue-grey darken-2">
+        <div class="card-content white-text">
+        <img src="https://picsum.photos/100" alt="">
+          <span class="card-title">Antra naujiena</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet at nobis assumenda blanditiis ducimus, facilis placeat. Minima minus voluptatem, molestiae ea necessitatibus, veritatis sint velit aliquid in, dolor nemo!.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">linkas</a>
+          <a href="#">web linkas</a>
+        </div>
+      </div>
+
+      <div class="card blue-grey darken-4">
+        <div class="card-content white-text">
+        <img src="https://picsum.photos/100" alt="">
+          <span class="card-title">Trecia naujiena</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem minima perspiciatis molestiae in iste hic harum molestias placeat eveniet magnam repellat, eligendi id dolore. Eos dicta similique labore aliquid beatae?.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">linkas</a>
+          <a href="#">web linkas </a>
+        </div>
+      </div>
+
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+        <img src="https://picsum.photos/100" alt="">
+          <span class="card-title">Ketvirta naujiena</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem minima perspiciatis molestiae in iste hic harum molestias placeat eveniet magnam repellat, eligendi id dolore. Eos dicta similique labore aliquid beatae?.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">linkas</a>
+          <a href="#">web linkas </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+    
+</body>
+</html>
+
+
 
 <?php include "footer.php" ?>

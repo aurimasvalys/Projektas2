593b33f03f6645f06dcada63fdf5e002c9e8a098
aurimasvalys <aurimasvalys@gmail.com>
2020-04-23 20:04:04 +0300

    atnaujinti headeriai breadcrumbsai aboutus

--- aboutus.php
+++ aboutus.php
@@ -1,8 +1,8 @@
 <?php include "header.php" ?>
-<link rel="stylesheet" href="styles/style.css">
+
 
 <nav class="teal lighten-4">
-  <div class="nav-wrapper">
+  <div class="container nav-wrapper">
     <div class="col s12 ">
       <a href="index.php" class="breadcrumb black-text">Index</a>
       <a href="visosnaujienos.php" class="breadcrumb black-text">News</a>
@@ -12,25 +12,26 @@
 </nav>
 
 <!-- About us puslapio tabai -->
+
 <div id="aboutusheader">
-
 <h1 id="aboutush1">About us</h1>
-
 </div>
 
-
+<div id="aboutustabsai">
 <ul class="tabs">
     <li class="tab col s3"><a class="active" href="#about_tab_1">Vision</a></li>
     <li class="tab col s3"><a href="#about_tab_2">Career</a></li>
     <li class="tab col s3"><a href="#about_tab_3">Contacts</a></li>
 </ul>
+</div>
+
 <div id="about_tab_1" class="col s12">
     <div class="container">
-        <div class="card">
+        <div class="card teal lighten-4">
             <div class="card-content">
-                <h4>Our vision:</h4>
+                <h4 class="center"> <i class="material-icons">airplanemode_active</i> Our vision:</h4>
                 <br><br>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea doloremque ducimus nemo enim quam veniam, quaerat, quod eveniet fugiat ullam explicabo ipsum. Ullam ab asperiores voluptatem voluptates ut sunt nobis?</p>
+                <p class="center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea doloremque ducimus nemo enim quam veniam, quaerat, quod eveniet fugiat ullam explicabo ipsum. Ullam ab asperiores voluptatem voluptates ut sunt nobis?</p>
                 <br><br>
                 <ul class="collapsible">
                     <li>
@@ -49,11 +50,11 @@
 </div>
 <div id="about_tab_2" class="col s12">
     <div class="container">
-        <div class="card">
+        <div class="card cyan lighten-4">
             <div class="card-content">
-                <h4>Career</h4>
+                <h4 class="center"> <i class="material-icons">device_hub</i> Career</h4>
                 <br><br>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora sit deleniti deserunt eius assumenda consequuntur suscipit, iure dolores voluptatibus necessitatibus autem sint sequi eum asperiores? Eum ut odit voluptate id?</p>
+                <p class="center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora sit deleniti deserunt eius assumenda consequuntur suscipit, iure dolores voluptatibus necessitatibus autem sint sequi eum asperiores? Eum ut odit voluptate id?</p>
                 <br><br>
                 <ul class="collapsible">
                     <li>
@@ -67,11 +68,11 @@
 </div>
 <div id="about_tab_3" class="col s12">
     <div class="container">
-        <div class="card">
+        <div class="card lime lighten-4">
             <div class="card-content">
-                <h3>Contacts</h3>
+                <h3 class="center"> <i class="material-icons">contacts</i> Contacts</h3>
                 <br><br>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio aperiam molestiae culpa a numquam iusto, mollitia ducimus voluptatem recusandae odio, in ut atque. Quisquam minima, alias sapiente consectetur error saepe!</p>
+                <p class="center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio aperiam molestiae culpa a numquam iusto, mollitia ducimus voluptatem recusandae odio, in ut atque. Quisquam minima, alias sapiente consectetur error saepe!</p>
                 <br><br>
                 <ul class="collapsible">
                     <li>

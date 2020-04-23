7348424225c91bdc89ebf1c211ed9fb68c6817d1
TomasPac <tompacesa@gmail.com>
2020-04-23 18:31:07 +0300

    About us puslapio forma, tabai, card, .js failo pildymas

--- aboutus.php
+++ aboutus.php
@@ -1,10 +1,75 @@
-
 <?php include "header.php" ?>
+<link rel="stylesheet" href="styles/style.css">
+
+<h4 class="center">About us</h4>
+<br>
+
+<!-- About us puslapio tabai -->
 
 
+<ul class="tabs">
+    <li class="tab col s3"><a class="active" href="#about_tab_1">Vision</a></li>
+    <li class="tab col s3"><a href="#about_tab_2">Career</a></li>
+    <li class="tab col s3"><a href="#about_tab_3">Contacts</a></li>
+</ul>
+<div id="about_tab_1" class="col s12">
+    <div class="container">
+        <div class="card">
+            <div class="card-content">
+                <h4>Our vision:</h4>
+                <br><br>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea doloremque ducimus nemo enim quam veniam, quaerat, quod eveniet fugiat ullam explicabo ipsum. Ullam ab asperiores voluptatem voluptates ut sunt nobis?</p>
+                <br><br>
+                <ul class="collapsible">
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">play_circle_outline</i>Video presentation</div>
 
-<!-- Čia About Us puslapio vidinė dalis -->
-
-
+                        <div class="collapsible-body"><span>
+                                <div class="video-container">
+                                    <iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                            </span></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="about_tab_2" class="col s12">
+    <div class="container">
+        <div class="card">
+            <div class="card-content">
+                <h4>Career</h4>
+                <br><br>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora sit deleniti deserunt eius assumenda consequuntur suscipit, iure dolores voluptatibus necessitatibus autem sint sequi eum asperiores? Eum ut odit voluptate id?</p>
+                <br><br>
+                <ul class="collapsible">
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">account_box</i>Sign Up</div>
+                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="about_tab_3" class="col s12">
+    <div class="container">
+        <div class="card">
+            <div class="card-content">
+                <h3>Contacts</h3>
+                <br><br>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio aperiam molestiae culpa a numquam iusto, mollitia ducimus voluptatem recusandae odio, in ut atque. Quisquam minima, alias sapiente consectetur error saepe!</p>
+                <br><br>
+                <ul class="collapsible">
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">location_on</i>Location</div>
+                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include "footer.php" ?>
--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,31 @@
 /* Tomas Pacesa script kodas: */
 
 
+// Tabs inicializacija 
+
+var scrollspies = document.querySelectorAll('.scrollspy')
+for (var i = 0; i < scrollspies.length; i++) {
+    M.ScrollSpy.init(scrollspies[i]);
+}
+
+var tabs = document.querySelectorAll('.tabs')
+for (var i = 0; i < tabs.length; i++) {
+    M.Tabs.init(tabs[i]);
+}
+
+// Tabs swipe scriptas 
+
+M.Tabs.init(document.querySelectorAll('.tabs'), { swipeable: true });
+
+// Colapsinimo scriptas 
+
+var collapsibles = document.querySelectorAll('.collapsible')
+for (var i = 0; i < collapsibles.length; i++) {
+    M.Collapsible.init(collapsibles[i]);
+}
+
+
+
 
 
 

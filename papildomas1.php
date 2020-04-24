7dec89a55bbacfb437806f65751132a0e44ea912
aurimasvalys <aurimasvalys@gmail.com>
2020-04-24 21:13:53 +0300

    fl

--- papildomas1.php
+++ papildomas1.php
@@ -54,11 +54,11 @@
 <div id="section2pp1">
 
 <div class="carousel">
-    <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
-    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
-    <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
-    <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
-    <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+    <a class="carousel-item" href="#one!"><img src="images\bp3.jpeg"></a>
+    <a class="carousel-item" href="#two!"><img src="images\bp4.jpeg"></a>
+    <a class="carousel-item" href="#three!"><img src="images\bp5.jpg"></a>
+    <a class="carousel-item" href="#four!"><img src="images\bp6.jpg"></a>
+    <a class="carousel-item" href="#five!"><img src="images\bp2.jpeg"></a>
   </div>
 
 

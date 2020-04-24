e45a387b9e0d86a42a929aa15df887aabaa0897f
aurimasvalys <aurimasvalys@gmail.com>
2020-04-24 17:19:38 +0300

    pataisymai

--- footer.php
+++ footer.php
@@ -1,33 +1,27 @@
     <div class="apacia">
-       
-    <div class="container">
-        <div class="row ">
-            <div class="col s4">
 
-            <img  src="dizainai\sierra\img\footer-logo.png" alt="">
-            <p class="white-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere animi dolores voluptatem maiores cupiditate unde laboriosam sunt fugiat repellat error nihil quia ipsum, ab, autem, consequatur sed debitis aperiam quo?</p>
+        <div class="container">
+            <div class="row ">
+                <div class="col s4">
 
+                    <img src="dizainai\sierra\img\footer-logo.png" alt="">
+                    <p class="white-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere animi dolores voluptatem maiores cupiditate unde laboriosam sunt fugiat repellat error nihil quia ipsum, ab, autem, consequatur sed debitis aperiam quo?</p>
+
+                </div>
+                <div class="col s4 center-align"> </div>
+                <div class="col s4 right-align">
+                    <ul id="apacialinks">
+                        <h5 id="linksh5">Follow us:</h5>
+                        <li><a href="#"><img src="images\facebook-circle.png" alt="" width="25"></a>Facebook</li>
+                        <li><a href="#"><img src="images\twitter-circle.png" alt="" width="25"></a>Twitter</li>
+                        <li><a href="#"><img src="images\linkedin-circle.png" alt="" width="25"></a>Linkedin</li>
+                        <li><a href="#"><img src="images\instagram-circle.png" alt="" width="25"></a>Instagram</li>
+                    </ul>
+                </div>
             </div>
-            <div class="col s4 center-align"> </div>
-            <div class="col s4 right-align">
-            <ul id="apacialinks">
-                <h5 id="linksh5">Follow us:</h5>
-                    <li><a href="#"><img src="images\facebook-circle.png" alt="" width="25"></a>Facebook</li>
-                    <li><a href="#"><img src="images\twitter-circle.png" alt="" width="25"></a>Twitter</li>
-                    <li><a href="#"><img src="images\linkedin-circle.png" alt="" width="25"></a>Linkedin</li>
-                    <li><a href="#"><img src="images\instagram-circle.png" alt="" width="25"></a>Instagram</li>
-                </ul>
-            </div>
+
+
         </div>
-       
-
-    </div>
-        
-
-    
-
-        
-    
 
     </div>
 

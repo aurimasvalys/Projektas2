ad4445ac9ef5e449c0057d893e5f43d583d7e552
Aurimas-Samirua <asurgelas@gmail.com.>
2020-04-24 21:47:17 +0300

    panaikinta nuoroda gale teksto

--- visosnaujienos.php
+++ visosnaujienos.php
@@ -24,15 +24,16 @@
 
       <div class=dalis1>
         <h3 id="let">Sportas</h3>
-        <p class="aprasymas"> >>>>>  <a class="sps" href="https://www.lrt.lt/tema/fake-news" target="_blank">Lionel Messi palieka Barcelona ...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+        <p class="aprasymas">  ><b class="naujausios">Naujausios</b>>>>>  <a class="sps" href="https://www.lrt.lt/tema/fake-news" target="_blank">Lionel Messi palieka Barcelona ...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
           quia
           accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
-          perspiciatis. Autem ea perspiciatis sunt? <a href="">Read more &rarr;</a> </p>
+          perspiciatis. Autem ea perspiciatis sunt? </p> 
+          <!-- <a href="">Read more &rarr;</a> -->
         <br>
-        <p class="aprasymas">>>>>> <a class="sps" href="">Eurolyga skelbia sezono pabaiga...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+        <p class="aprasymas">><b class="naujausios">Naujausios</b>>>>> <a class="sps" href="">Eurolyga skelbia sezono pabaiga...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
           quia
           accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
-          perspiciatis. Autem ea perspiciatis sunt? <a href="">Read more &rarr;</a> </p>
+          perspiciatis. Autem ea perspiciatis sunt?  </p>
           
       </div>
       <div><a href="vienanaujiena.php" id="sportasmygtukas" class="waves-effect waves-light btn">Open</a></div>
@@ -46,15 +47,15 @@
 
       <div class=dalis1>
         <h3 id="let">Politika</h3>
-        <p class="aprasymas">>>>>> <a class="sps" href="">Lietuva renka prezidentą ...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+        <p class="aprasymas">><b class="naujausios">Naujausios</b>>>>> <a class="sps" href="">Lietuva renka prezidentą ...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
           quia
           accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
-          perspiciatis. Autem ea perspiciatis sunt? <a href="">Read more &rarr;</a> </p>
+          perspiciatis. Autem ea perspiciatis sunt?  </p>
         <br>
-        <p class="aprasymas">>>>>><a class="sps" href="">Kinija paskelbė karantiną...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+        <p class="aprasymas">><b class="naujausios">Naujausios</b>>>>><a class="sps" href="">Kinija paskelbė karantiną...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
           quia
           accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
-          perspiciatis. Autem ea perspiciatis sunt? <a href="">Read more &rarr;</a> </p>
+          perspiciatis. Autem ea perspiciatis sunt?  </p>
        
 
       </div>
@@ -69,15 +70,15 @@
 
       <div class=dalis1>
         <h3 id="let">Laisvalaikis</h3>
-        <p class="aprasymas">>>>>><a class="sps" href="">Keliajame į Egiptą...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+        <p class="aprasymas">><b class="naujausios">Naujausios</b>>>>><a class="sps" href="">Keliajame į Egiptą...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
           quia
           accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
-          perspiciatis. Autem ea perspiciatis sunt? <a href="">Read more &rarr;</a> </p>
+          perspiciatis. Autem ea perspiciatis sunt?  </p>
         <br>
-        <p class="aprasymas">>>>>><a class="sps" href="">Egzotinės šalys...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
+        <p class="aprasymas">><b class="naujausios">Naujausios</b>>>>><a class="sps" href="">Egzotinės šalys...</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, nostrum cumque
           quia
           accusantium ab, libero animi iure tempora exercitationem sint modi incidunt maiores similique, dicta
-          perspiciatis. Autem ea perspiciatis sunt? <a href="">Read more &rarr;</a> </p>
+          perspiciatis. Autem ea perspiciatis sunt? </p>
       </div>
       <div><a href="vienanaujiena.php" id="laisvalaikismygtukas" class="waves-effect waves-light btn">Open</a></div>
     </div>

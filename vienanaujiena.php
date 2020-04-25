de7e08bdc569ea2f4262fecd0acb6263ecda2e8f
aurimasvalys <aurimasvalys@gmail.com>
2020-04-26 00:07:53 +0300

    w3 changes breadcrumbs vienanaujiena

--- vienanaujiena.php
+++ vienanaujiena.php
@@ -1,13 +1,15 @@
-
 <?php include "header.php" ?>
 
 <nav class="teal lighten-4">
-    <div class="container nav-wrapper">
-        <div class="col s12 ">
-            <a href="index.php" class="breadcrumb black-text">Index</a>
-            <a href="vienanaujiena.php" class="breadcrumb black-text">News 1</a>
-        </div>
+  <div class="container nav-wrapper">
+    <div class="col s12 ">
+      <div class="w3-animate-left">
+        <a href="index.php" class="breadcrumb black-text">Index</a>
+        <a href="vienanaujiena.php" class="breadcrumb black-text">News 1</a>
+      </div>
+
     </div>
+  </div>
 </nav>
 
 
@@ -15,55 +17,56 @@
 
 
 <div class="fonas row container">
-   <img class = " aukstis col s12" src="\images\header.jpg" alt="">
-   <div>Lorem ip"sum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+  <img class=" aukstis col s12" src="\images\header.jpg" alt="">
+  <div>Lorem ip"sum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
 
-   </div>
-
-  
-
-</div >
-
-  
-
-    <div class="apacia1 row container ">
-    <div class=" row ">
-      <div class="col s8  m6  card blue-grey darken-2">
-        <div class="card-content white-text">
-        <img src="https://picsum.photos/seed/1/100" alt="">
-          <span class="card-title">Orai</span>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet at nobis assumenda blanditiis ducimus, facilis placeat. Minima minus voluptatem, molestiae ea necessitatibus, veritatis sint velit aliquid.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">linkas</a>
-          <a href="#">web linkas</a>
-        </div>
-      </div>
-
-      <div class="col s8 m6 card blue-grey darken-4">
-        <div class="card-content white-text">
-        <img src="https://picsum.photos/seed/2/100" alt="">
-          <span class="card-title">Vieta jusu reklamai</span>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem minima perspiciatis molestiae in iste hic harum molestias placeat eveniet magnam repellat, eligendi id dolore. Eos dicta similique labore aliquid beatae?.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">linkas</a>
-          <a href="#">web linkas </a>
-        </div>
-      </div>
-
-       </div>
   </div>
 
 
-    
+
+</div>
+
+
+
+<div class="apacia1 row container ">
+  <div class=" row ">
+    <div class="col s8  m6  card blue-grey darken-2">
+      <div class="card-content white-text">
+        <img src="https://picsum.photos/seed/1/100" alt="">
+        <span class="card-title">Orai</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet at nobis assumenda blanditiis ducimus, facilis placeat. Minima minus voluptatem, molestiae ea necessitatibus, veritatis sint velit aliquid.</p>
+      </div>
+      <div class="card-action">
+        <a href="#">linkas</a>
+        <a href="#">web linkas</a>
+      </div>
+    </div>
+
+    <div class="col s8 m6 card blue-grey darken-4">
+      <div class="card-content white-text">
+        <img src="https://picsum.photos/seed/2/100" alt="">
+        <span class="card-title">Vieta jusu reklamai</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem minima perspiciatis molestiae in iste hic harum molestias placeat eveniet magnam repellat, eligendi id dolore. Eos dicta similique labore aliquid beatae?.</p>
+      </div>
+      <div class="card-action">
+        <a href="#">linkas</a>
+        <a href="#">web linkas </a>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+
 </body>
+
 </html>
 
 

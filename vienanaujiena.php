d8da804f5745243d289d8efa805fc9570e01af79
aurimasvalys <aurimasvalys@gmail.com>
2020-04-27 11:14:13 +0300

    vienanaujiena.php korekcijos

--- vienanaujiena.php
+++ vienanaujiena.php
@@ -13,10 +13,10 @@
 </nav>
 
 
-<h1 class="antraste container">Naujienos turinys</h1>
+<h1 class="antraste container w3-animate-right">Naujienos turinys</h1>
 
 
-<div class="fonas row container">
+<div class="fonas row container w3-animate-top">
   <img class=" aukstis col s12" src="\images\header.jpg" alt="">
   <div>Lorem ip"sum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sit laborum harum nesciunt voluptate aspernatur! Odio, atque ratione quas magni, adipisci sint maiores officia vel ipsum, deleniti earum quod!
@@ -36,7 +36,7 @@
 
 <div class="apacia1 row container ">
   <div class=" row ">
-    <div class="col s8  m6  card blue-grey darken-2">
+    <div class="w3-animate-left col s8  m6  card blue-grey darken-2">
       <div class="card-content white-text">
         <img src="https://picsum.photos/seed/1/100" alt="">
         <span class="card-title">Orai</span>
@@ -48,7 +48,7 @@
       </div>
     </div>
 
-    <div class="col s8 m6 card blue-grey darken-4">
+    <div class="w3-animate-right col s8 m6 card blue-grey darken-4">
       <div class="card-content white-text">
         <img src="https://picsum.photos/seed/2/100" alt="">
         <span class="card-title">Vieta jusu reklamai</span>
